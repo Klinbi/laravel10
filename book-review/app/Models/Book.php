8c2895d4a0bbb47527750bbc80d60bc6da270fc2
Vitezslav Linke <vlinke@Vitezslavs-MacBook-Pro.local>
2024-05-08 22:15:04 +0200

    Add book review funcionality

---
+++ book-review/app/Models/Book.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    use HasFactory;
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
+
+    public function scopeTitle(Builder $query, string $title): Builder
+    {
+        return $query->where('title', 'LIKE', '%' . $title . '%');
+    }
+
+    public function scopePopular(Builder $query, $from = null, $to = null): Builder
+    {
+        return $query->withCount([
+            'reviews' => fn (Builder $q) => $this->dateRangeFilter($q, $from, $to)
+            ])->orderBy('reviews_count', 'desc');
+    }
+
+    public function scopeHighestRated(Builder $query, $from = null, $to = null) : Builder
+    {
+        return $query->withAvg([
+            'reviews' => fn (Builder $q) => $this->dateRangeFilter($q, $from, $to)
+            ], 'rating')
+            ->orderBy('reviews_avg_rating', 'desc');
+    }
+
+    public function scopeMinReviews(Builder $query, int $minReviews) : Builder
+    {
+        return $query->having('reviews_count', '>=', $minReviews);
+    }
+
+    private function dateRangeFilter(Builder $query, $from = null, $to = null)
+    {
+        if ($from && !$to) {
+            $query->where('created_at', '>=', $from);
+        } elseif (!$from && $to) {
+            $query->where('created_at', '<=', $to);
+        } elseif ($from && $to) {
+            $query->whereBetween('created_at', [$from, $to]);
+        }
+    }
+
+}
--- book-review/database/seeders/DatabaseSeeder.php
+++ book-review/database/seeders/DatabaseSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Book;
+use App\Models\Review;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +14,31 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        Book::factory(33)->create()->each(function ($book) {
+            $numReviews = random_int(5, 30);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+            Review::factory()->count($numReviews)
+                ->good()
+                ->for($book)
+                ->create();
+        });
+
+        Book::factory(33)->create()->each(function ($book) {
+            $numReviews = random_int(5, 30);
+
+            Review::factory()->count($numReviews)
+                ->average()
+                ->for($book)
+                ->create();
+        });
+
+        Book::factory(34)->create()->each(function ($book) {
+            $numReviews = random_int(5, 30);
+
+            Review::factory()->count($numReviews)
+                ->bad()
+                ->for($book)
+                ->create();
+        });
     }
 }
